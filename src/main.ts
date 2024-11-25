48b790eb3c5d6c1125dcad5949eee14e5b8833d7
Edward Mizuka <mizuka.wu@outlook.com>
2024-11-25 21:40:01 +0800

    feat: message.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,10 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { Message } from "./Message";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const message = new Message({
+  message: "这是一个消息",
+  type: "success",
+  icon: "/path/to/icon.png",
+  showClose: true,
+  duration: 3000,
+  appendTo: document.body,
+});

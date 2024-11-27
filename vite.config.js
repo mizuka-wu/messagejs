bc742917641e2e235ec61fa91f4d8d4704c1d731
Edward Mizuka <mizuka.wu@outlook.com>
2024-11-27 11:17:37 +0800

    build: 添加类型声明文件路径和配置优化

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import dts from 'vite-plugin-dts';
 
 export default defineConfig({
     plugins: [dts({
-        insertTypesEntry: true
+        insertTypesEntry: true,
+        include: ["./src"],
+        // rollupType: true
     })],
     build: {
         lib: {

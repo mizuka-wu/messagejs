5e302ac4494d7712934d8cd0a21cac28d1db520c
Edward Mizuka <mizuka.wu@outlook.com>
2024-11-27 11:09:31 +0800

    fix: 修复了package-lock.json文件中的错误

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'SimpleMessage',
-      // 将添加适当的扩展名后缀
-      fileName: 'simple-message',
+    plugins: [dts({
+        insertTypesEntry: true
+    })],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'SimpleMessage',
+            // 将添加适当的扩展名后缀
+            fileName: 'simple-message',
+        },
+        rollupOptions: {
+            output: {
+                exports: 'named'
+            }
+        },
     },
-    rollupOptions: {
-        output: {
-            exports: 'named'
-        }
-    },
-  },
 })
